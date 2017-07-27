d4ef339771ed368fa3b245d981ef586ec0d09790
chenyantaoaaa <334632474@qq.com>
2017-07-27 16:35:42 +0800

    图片压缩

---
+++ com.cyt.music.interfaces/src/main/java/com/cyt/music/interfaces/pojo/system/EmailDTO.java
@@ -0,0 +1,47 @@
+package com.cyt.music.interfaces.pojo.system;
+
+/**
+ * @author yantao.chen
+ * @date 2017/7/27.
+ */
+public class EmailDTO {
+    private String name;
+
+    private String phone;
+
+    private String email;
+
+    private String content;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
--- com.cyt.music.webapp/src/main/java/controller/email/emilController.java
+++ com.cyt.music.webapp/src/main/java/controller/email/emilController.java
@@ -1,13 +1,13 @@
 package controller.email;
 
 
+import com.cyt.music.interfaces.pojo.system.EmailDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import service.email.EmailService;
-import service.order.OrderInfoService;
 
 /**
  * Created by chen on 2017/7/9.
@@ -21,10 +21,13 @@ public class emilController {
 
     @RequestMapping(value = "/sendemail")
     public @ResponseBody
-    void queryOrderInfo() throws Exception {
-        String title="我是一封test邮件";
-        String emailStr="test邮件";
-        emailService.sendEmail(emailStr,title);
+    void queryOrderInfo(@RequestBody EmailDTO dto) throws Exception {
+        String title="客户意见反馈";
+
+        StringBuilder emailStr=new StringBuilder();
+        emailStr.append("客户名:").append(dto.getName()).append("  ").append("邮箱地址:").append(dto.getEmail()).append("  ")
+                .append("客户电话:").append(dto.getPhone()).append("  ").append("意见内容:").append(dto.getContent());
+        emailService.sendEmail(emailStr.toString(),title);
     }
 }
 
